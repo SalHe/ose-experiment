2f886709db70fb330d802a62b3cb525bfd60cfe6
SalHe <SalHe@qq.com>
2021-07-02 12:50:43 +0800

    更新实验四

--- inLinux/E04Fork.cpp
+++ inLinux/E04Fork.cpp
@@ -7,7 +7,18 @@ int main(int argc, char const *argv[])
     if (pid < 0)
         printf("创建失败\n");
     else if (pid > 0)
-        printf("B,C\n");
+    {
+        printf("B\n");
+        pid = fork();
+        if (pid < 0)
+        {
+            printf("从B创建子进程失败！\n");
+        }
+        else if (pid > 0)
+        {
+            printf("C\n");
+        }
+    }
     else // pid == 0
         printf("A\n");
     return 0;
