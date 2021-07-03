8cad51ca35367cacaa1a5407b5147ee74f4d0d85
SalHe <SalHe@qq.com>
2021-07-03 17:06:34 +0800

    修正父子进程问题
    把父子进程搞反了

--- inLinux/E04Fork.cpp
+++ inLinux/E04Fork.cpp
@@ -8,18 +8,20 @@ int main(int argc, char const *argv[])
         printf("创建失败\n");
     else if (pid > 0)
     {
-        printf("B\n");
+        printf("A, 子进程ID：%d\n", pid);
+    }
+    else // pid == 0
+    {
+        printf("B, 我是A创建的子进程，我即将创建进程C\n");
         pid = fork();
         if (pid < 0)
         {
             printf("从B创建子进程失败！\n");
         }
-        else if (pid > 0)
+        else if (pid == 0)
         {
-            printf("C\n");
+            printf("C, 我是B创建的子进程\n");
         }
     }
-    else // pid == 0
-        printf("A\n");
     return 0;
 }
