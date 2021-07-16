ba28aa6003189b4cf420d07f5ffcbd2fa93d0dc8
SalHe <SalHe@qq.com>
2021-07-16 08:55:18 +0800

    update

--- inLinux/E04Fork.cpp
+++ inLinux/E04Fork.cpp
@@ -12,16 +12,23 @@ int main(int argc, char const *argv[])
     }
     else // pid == 0
     {
-        printf("B, 我是A创建的子进程，我即将创建进程C\n");
+        printf("B, 我是A创建的子进程\n");
+    }
+
+    if (pid > 0)
+    {
         pid = fork();
         if (pid < 0)
+            printf("创建失败\n");
+        else if (pid > 0)
         {
-            printf("从B创建子进程失败！\n");
+            printf("A, 子进程ID：%d\n", pid);
         }
-        else if (pid == 0)
+        else // pid == 0
         {
-            printf("C, 我是B创建的子进程\n");
+            printf("C, 我是A创建的子进程\n");
         }
     }
+
     return 0;
 }
